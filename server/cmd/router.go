bd2cc3ddf413540e34a3eae18386e08b4ac3bdce
ena141 <takina141@gmail.com>
2024-09-06 23:01:24 +0900

    Add router

---
+++ server/cmd/router.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"Nightcord/server/internal/config"
+	"github.com/gin-gonic/gin"
+)
+
+func initRouter(cfg config.Config) {
+
+	// init router
+	router := gin.Default()
+
+}
