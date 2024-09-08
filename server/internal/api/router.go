db29cf0b51ba04be3cb34748a5740fbe62c0ad2e
ena141 <takina141@gmail.com>
2024-09-08 12:40:21 +0900

    Set the router

--- server/internal/api/router.go
+++ server/internal/api/router.go
@@ -6,11 +6,23 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Handler struct {
+	userHandler *UserHandler
+}
+
 func SetupRouter(services *services.Services, cfg *config.Config) *gin.Engine {
-	services.UserService.GetUser(cfg.DatabaseUrl)
+	userHandler := NewUserHandler(services)
+	handler := &Handler{
+		userHandler: userHandler,
+	}
 
 	// init router
 	router := gin.Default()
+
+	router.Group("/user")
+	{
+		router.GET("/:id", handler.userHandler.GetUser)
+	}
 
 	return router
 }
--- server/internal/api/user.go
+++ server/internal/api/user.go
@@ -1,9 +1,18 @@
 package api
 
 import (
+	"Nightcord/server/internal/services"
 	"github.com/gin-gonic/gin"
 )
 
-func InitUserHandler(r *gin.Engine) {
+type UserHandler struct {
+	userService services.UserService
+}
 
+func NewUserHandler(services *services.Services) *UserHandler {
+	return &UserHandler{userService: services.UserService}
+}
+
+func (UserHandler *UserHandler) GetUser(c *gin.Context) {
+	UserHandler.userService.GetUser("123")
 }
